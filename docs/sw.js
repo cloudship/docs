712537b328e81070c004ff9423206b7e982bc107
cloudship <cloudship@gmail.com>
2018-08-29 07:21:54 +0800

    add support for auto paste from clipboard, support chrome 66+

--- docs/sw.js
+++ docs/sw.js
@@ -1,24 +1,47 @@
-self.addEventListener("install", function(event){
+const CACHE_NAME = "v33";
+const cacheFiles = [
+  "/docs/",
+  "/docs/index.html",
+  "/docs/bundle.js"
+]
+
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open("v25").then(function(cache){
-      console.log("successful cache the files");
-      return cache.addAll([
-        "/docs/",
-        "/docs/index.html",
-        "/docs/bundle.js"
-      ])
-    }).catch(function(err){
-      console.log("fail to load cache: " + err)
-    })
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log("start caching files: " + CACHE_NAME);
+        return cache.addAll(cacheFiles)
+      }, function (err) {
+        console.log("fail to open cache: " + err)
+      })
   )
 })
 
-self.addEventListener("fetch", function(event){
+self.addEventListener("fetch", function (event) {
   console.log("start loading cache content...")
   event.respondWith(
-    caches.match(event.request).then(function(response){
-      console.log("now loading: " + event.request.url)
-      return response || fetch(event.request)
+    caches.match(event.request).then(function (response) {
+      //in case there's a match
+      if (response) {
+        console.log("now loading: " + event.request.url)
+        return response;
+      }
+      //in case there's no match
+      let req = event.request.clone();
+      fetch(req).then(
+        function (response) {
+          if (!reponse || response.status != 200 || response.type !== 'basic') { return response }
+          let res = response.clone();
+          caches.open(CACHE_NAME)
+            .then(function (cache) {
+              console.log("start cahcing new files ...")
+              cache.put(event.request, res)
+            })
+          return response;
+        }).catch(function () {
+          console.log("unable to resolve the request.")
+          return "unable to resolve the request.";
+        })
     })
   )
 })
